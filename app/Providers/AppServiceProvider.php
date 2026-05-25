db84a421553407c106e42f5a30f96fc93260891b
FahmiGF <fahmige123@gmail.com>
2026-05-25 12:12:02 +0700

    Week 12 - Perbaikan Commit sementar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,13 @@
-<?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
+public function boot(): void
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
     {
         //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        Paginator::useBootstrapFive();
     }
 }
