440a500c9138c4b6bde23450828a1ee22dbab2fc
FahmiGF <fahmige123@gmail.com>
2026-06-08 16:53:08 +0700

    Latihan EAS

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController ;
 use App\Http\Controllers\PegawaiDBController ;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\keranjangController;
+
 
 
 Route::get('/', function () {
@@ -65,6 +69,25 @@ Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
+//crud tabel keranjangbelanja
+Route::get('/keranjangbelanja', [keranjangController::class, 'index_keranjang']);
+Route::get('/keranjangbeli/{id}', [keranjangController::class, 'beli']);
+Route::post('/keranjangstore', [keranjangController::class, 'storeKeranjang']);
+Route::get('/keranjangbatal/{id}', [keranjangController::class, 'batal']);
+
+
 //
 
+// jangan di replace file nya , copy paste kan perintahnya
+//use App\Http\Controllers\SiswaController;
 
+
+
+//route CRUD siswa
+/*Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+Route::get('/siswa/{nrp}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::put('/siswa/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+*/
